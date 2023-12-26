5976bf81d48999831f2a090f5601ec8cd7d3ce96
wnalsrjs229 <wnalsrjs229@naver.com>
2023-12-27 00:13:10 +0900

    feat:시기추가와새로고침수정

--- src/pages/Missions/Mission.jsx
+++ src/pages/Missions/Mission.jsx
@@ -8,11 +8,18 @@ function Mission() {
   const [message, setMessage] = useState("Mission");
 
   const numbers = [1, 2, 3, 4];
-  const [number, setNumber] = useState(3);
+  const [number, setNumber] = useState(1);
+
+  const periods = ["과거", "현재", "미래"];
+  const [period, setPeriod] = useState("현재");
 
   const changeMessage = () => {
     const randomIndex = Math.floor(Math.random() * messages.length);
     setMessage(messages[randomIndex]);
+  };
+
+  const handlePeriodChange = (newPeriod) => {
+    setPeriod(newPeriod);
   };
 
   return (
@@ -22,15 +29,32 @@ function Mission() {
         <img src={photoForNav} />
       </div>
       <div className="center-container">
-        <div className="question">어떤 미션을 수행하시겠습니까?</div>
-        <div className="mission-container">{message}</div>
-        <div className="change-container">
-          <button onClick={changeMessage} className="changeButton">
-            <LuRefreshCcw />
+        <div style={{ width: "71%", marginTop: "100px" }}>
+          <span className="question">어떤 미션을 수행하시겠습니까?</span>
+          <button onClick={() => handlePeriodChange("과거")} className="Button">
+            과거
           </button>
-          새로운 미션
+          /
+          <button onClick={() => handlePeriodChange("현재")} className="Button">
+            현재
+          </button>
+          /
+          <button onClick={() => handlePeriodChange("미래")} className="Button">
+            미래
+          </button>
+          <span style={{ fontSize: "x-large", fontWeight: "bold", float: "right" }}>#{period}</span>
         </div>
-        <div className="waiting-container">미공개 미션</div>
+        <span className="mission-container">{message}</span>
+        {number === 0 && (
+          <div className="change-container">
+            <button onClick={changeMessage} className="changeButton">
+              <LuRefreshCcw />
+            </button>
+            새로운 미션
+          </div>
+        )}
+
+        <h2 className="waiting-container">미공개 미션</h2>
         <div style={{ display: "flex", alignItems: "center" }}>
           인증 현황{" "}
           <div className="percentbar_out">
