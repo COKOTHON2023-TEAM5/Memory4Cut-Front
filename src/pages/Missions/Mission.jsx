9a6c71e1e9a3cad22c1be4fad649f9f1aa17d7f0
wnalsrjs229 <wnalsrjs229@naver.com>
2023-12-27 02:15:33 +0900

    feat:두번째해쉬태그추가와현황위치옮김

--- src/pages/Missions/Mission.jsx
+++ src/pages/Missions/Mission.jsx
@@ -5,21 +5,30 @@ import { LuRefreshCcw } from "react-icons/lu";
 
 function Mission() {
   const messages = ["Mission1", "Mission2", "Mission3", "Mission4"];
-  const [message, setMessage] = useState("Mission");
+  const [message, setMessage] = useState("미공개 미션");
 
   const numbers = [1, 2, 3, 4];
-  const [number, setNumber] = useState(1);
+  const [number, setNumber] = useState(0);
 
-  const periods = ["과거", "현재", "미래"];
-  const [period, setPeriod] = useState("현재");
+  const periods1 = ["과거", "현재", "미래"];
+  const [period1, setPeriod1] = useState("");
+
+  const periods2 = ["과거", "현재", "미래"];
+  const [period2, setPeriod2] = useState("");
 
   const changeMessage = () => {
     const randomIndex = Math.floor(Math.random() * messages.length);
     setMessage(messages[randomIndex]);
+    changePeriod();
   };
 
   const handlePeriodChange = (newPeriod) => {
-    setPeriod(newPeriod);
+    setPeriod1(newPeriod);
+  };
+
+  const changePeriod = () => {
+    const randomIndex = Math.floor(Math.random() * periods2.length);
+    setPeriod2(periods2[randomIndex]);
   };
 
   return (
@@ -42,26 +51,27 @@ function Mission() {
           <button onClick={() => handlePeriodChange("미래")} className="Button">
             미래
           </button>
-          <span style={{ fontSize: "x-large", fontWeight: "bold", float: "right" }}>#{period}</span>
+          <span style={{ fontSize: "x-large", fontWeight: "bold", float: "right" }}>#{period1}</span>
         </div>
-        <span className="mission-container">{message}</span>
-        {number === 0 && (
-          <div className="change-container">
-            <button onClick={changeMessage} className="changeButton">
-              <LuRefreshCcw />
-            </button>
-            새로운 미션
-          </div>
-        )}
-
-        <h2 className="waiting-container">미공개 미션</h2>
-        <div style={{ display: "flex", alignItems: "center" }}>
+        <span className="mission-container">Mission</span>
+        <div style={{ display: "flex", alignItems: "center", float: "right", width: "71%", justifyContent: "end", marginBottom: "60px" }}>
           인증 현황{" "}
           <div className="percentbar_out">
             <div className="percentbar_in" style={{ width: `${25 * number}%` }}></div>
           </div>
           ({number}/4)
+          {number === 0 && (
+            <div className="change-container">
+              <button onClick={changeMessage} className="changeButton">
+                <LuRefreshCcw />
+              </button>
+              새로운 미션
+            </div>
+          )}
+          {number !== 0 && <div className="change-container"></div>}
         </div>
+        <div className="hashtag">#{period2}</div>
+        <h2 className="waiting-container">{message}</h2>
       </div>
     </div>
   );
