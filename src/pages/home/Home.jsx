a4da6a031000f37f7d79234c31e5dff270fe4452
YimTaekeun <ytk030305@gmail.com>
2023-12-26 22:53:54 +0900

    [FEAT] 팝업창 css 추가

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import Modal from 'react-modal';
+import { useState } from "react";
 import './Home.css';
 import photoForNav from './imgSrc/photoForNav.png';
 import fourCutForBody from './imgSrc/fourCutForBody.png';
 const Home = () => {
     const goPage = useNavigate();
+    const[modalOpen, setModalOpen] = useState(false);
     return <div className="Home">
         <div className="nav">
             <h1 style={{fontFamily: "Gowun Batang"}}>추억네컷</h1>
@@ -16,7 +19,9 @@ const Home = () => {
                     <input placeholder="입장할 그룹의 초대코드를 입력해주세요." style={{margin: "20px"}}/>
                 </div>
                 <button style={{fontFamily: "Gowun Batang"}} id="JoinGroupButton" onClick={() => {
-                    goPage("/JoinGroup");
+                    //TODO: db비교
+                    setModalOpen(true);
+                    // goPage("/JoinGroup");
                 }}>그룹 참여하기</button>
                 <p/>
                 <button style={{fontFamily: "Gowun Batang"}} id="GroupGeneratorButton" onClick={() => {
@@ -26,6 +31,13 @@ const Home = () => {
             <div className="bodyRight" style={{display: "flex"}}>
                 <img src={fourCutForBody}/>
             </div>
+            {/* 아래는 팝업창 화면 */}
+            <Modal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)}>
+                <div className="ModalBody">
+                    <h1>존재하지 않는 초대 코드 입니다.</h1>
+                    <button onClick={() => {setModalOpen(false);}} style={{marginTop: "70px"}}>확인</button>
+                </div>
+            </Modal>
         </div>
     </div>
 }
