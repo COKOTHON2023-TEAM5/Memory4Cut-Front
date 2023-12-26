d1ce677630f29105b454e654e2c42c0337efb1d3
wnalsrjs229 <wnalsrjs229@naver.com>
2023-12-26 20:48:31 +0900

    fix : 코드 재수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import GroupGenerator from "./pages/groupGenrator/GroupGenerator";
 import MissionImagesUpload from "./pages/MissionImagesUpload/MissionImagesUpload";
+import Mission from "./pages/Missions/Mission";
 
 function App() {
   return (
@@ -12,10 +12,8 @@ function App() {
         <Routes>
           <Route path="/" Component={Home}></Route>
           <Route path="/GroupGenerator" Component={GroupGenerator}></Route>
-          <Route
-            path="/MissionImagesUpload"
-            Component={MissionImagesUpload}
-          ></Route>
+          <Route path="/MissionImagesUpload" Component={MissionImagesUpload}></Route>
+          <Route path="Mission" Component={Mission}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Missions/Mission.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./Mission.css";
+
+function Mission() {
+  const messages = ["Mission1", "Mission2", "Mission3", "Mission4"];
+
+  const [message, setMessage] = useState("Mission");
+
+  const changeMessage = () => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    setMessage(messages[randomIndex]);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="center-container">
+        <div className="question">어떤 미션을 수행하시겠습니까?</div>
+        <div className="mission-container">{message}</div>
+        <div className="change-container">
+          <button onClick={changeMessage} className="changeButton">
+            .
+          </button>
+          새로운 미션
+        </div>
+        <div className="waiting-container">다른 미션</div>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1 className="Header">추억네컷</h1>
+      <div className="HeaderImage"></div>
+    </header>
+  );
+}
+
+export default Mission;
